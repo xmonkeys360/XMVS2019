f68128c9bee95b577f2cdaecd9c922c485054635
Anbu Mani <anbumani91@hotmail.com>
2019-05-15 00:33:31 +0530

    Added Decks

---
+++ WhatsNew-In-Xamarin-Forms-VS2019/CollectionView/CollectionViewSample/CollectionViewSample/AppShell.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViewSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Navigating(object sender, Xamarin.Forms.ShellNavigatingEventArgs e)
+        {
+            if ((Device.RuntimePlatform == "iOS") && e.Target.Location.AbsolutePath.IndexOf("carousel") > -1)
+            {
+                (App.Current.MainPage as Shell).DisplayAlert(
+                    title: "Not Yet!",
+                    message: "iOS doesn't yet support CarouselView.",
+                    cancel: "Okay"
+                );
+                e.Cancel();
+            }
+        }
+    }
+}
